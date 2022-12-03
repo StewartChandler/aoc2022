48acac6aee63c2fa8727831588498f2b5db63d86
Stewart Chandler <stewartdchandler@gmail.com>
2022-12-03 16:44:29 -0500

    rustfmt or day3

--- src/d3/day3.rs
+++ src/d3/day3.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, fs::File, io::{BufRead, BufReader}};
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn priority(c: &u8) -> u8 {
     match *c {
@@ -8,7 +12,6 @@ fn priority(c: &u8) -> u8 {
     }
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     let fpath = std::env::args().nth(1).expect("no file argument found");
 
@@ -16,34 +19,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     for line in BufReader::new(File::open(fpath)?).lines() {
         if let Ok(s) = line {
             let b = s.as_bytes();
-            let (l, r) = b.split_at(b.len()/2);
-            rucksacks.push(l
-                .iter()
-                .zip(r)
-                .fold((0u64, 0u64), |(la, ra), (l, r)| {
-                    (la | 1u64 << priority(l), ra | 1u64 << priority(r))
-                })
-            );
+            let (l, r) = b.split_at(b.len() / 2);
+            rucksacks.push(l.iter().zip(r).fold((0u64, 0u64), |(la, ra), (l, r)| {
+                (la | 1u64 << priority(l), ra | 1u64 << priority(r))
+            }));
         }
     }
 
     // eprintln!("{:?}", rucksacks);
 
     // part 1
-    println!("Part 1: {}", rucksacks.iter()
-        .map(|(l, r)| (l & r).trailing_zeros())
-        .sum::<u32>()
+    println!(
+        "Part 1: {}",
+        rucksacks
+            .iter()
+            .map(|(l, r)| (l & r).trailing_zeros())
+            .sum::<u32>()
     );
 
     // part 2
     let mut sum = 0u32;
     let mut itr = rucksacks.iter().map(|(l, r)| (l | r));
     while let Some(val) = itr.next() {
-        sum += (
-            val & 
-            itr.next().unwrap_or(0) & 
-            itr.next().unwrap_or(0)
-        ).trailing_zeros();
+        sum += (val & itr.next().unwrap_or(0) & itr.next().unwrap_or(0)).trailing_zeros();
     }
     println!("Part 2: {}", sum);
 
