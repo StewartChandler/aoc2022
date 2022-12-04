cc0e4ac416c7bf92b8a4576f3014e85b6607ddd4
Stewart Chandler <stewartdchandler@gmail.com>
2022-12-04 00:24:36 -0500

    completed day 4

---
+++ src/d4/day4.rs
@@ -0,0 +1,49 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let fpath = std::env::args().nth(1).expect("no file argument found");
+
+    let mut pairs: Vec<[u32; 4]> = Vec::new();
+    for line in BufReader::new(File::open(fpath)?).lines() {
+        if let Ok(s) = line {
+            let nums: Vec<u32> = s
+                .split(&[',', '-'])
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect();
+
+            pairs.push(nums.try_into().unwrap());
+        }
+    }
+
+    // eprintln!("{:#?}", pairs);
+
+    // part 1
+    println!(
+        "Part 1: {}",
+        pairs.iter().fold(0, |sum, arr| {
+            if (arr[0] >= arr[2] && arr[1] <= arr[3]) || (arr[0] <= arr[2] && arr[1] >= arr[3]) {
+                sum + 1
+            } else {
+                sum
+            }
+        })
+    );
+
+    // part 2
+    println!(
+        "Part 2: {}",
+        pairs.iter().fold(0, |sum, arr| {
+            if arr[0] > arr[3] || arr[1] < arr[2] {
+                sum
+            } else {
+                sum + 1
+            }
+        })
+    );
+
+    Ok(())
+}
